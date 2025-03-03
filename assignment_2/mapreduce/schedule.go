ad924c89378a3a5b1e5890244440d6a89b65f21e
Ossama <ossamatammam@gmail.com>
2025-03-03 22:43:32 +0200

    Task 3 done without retires

--- assignment_2/mapreduce/schedule.go
+++ assignment_2/mapreduce/schedule.go
@@ -1,5 +1,26 @@
 package mapreduce
 
+func runTask(workerArgs RunTaskArgs, workerChan chan string, taskCompleteChan chan bool) {
+	workerAddress := <-workerChan
+	taskSuccess := call(workerAddress, "Worker.RunTask", &workerArgs, &struct{}{})
+	if taskSuccess {
+		taskCompleteChan <- true
+	}
+	workerChan <- workerAddress // Reuse workers
+}
+
+func monitorFileCompletion(nTasks int, taskCompleteChan chan bool, tasksDoneChan chan bool) {
+	for range taskCompleteChan {
+		nTasks--
+		if nTasks == 0 {
+			tasksDoneChan <- true
+			close(taskCompleteChan)
+			close(tasksDoneChan)
+			break
+		}
+	}
+}
+
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
 	var numOtherPhase int
@@ -14,6 +35,24 @@ func (mr *Master) schedule(phase jobPhase) {
 	debug("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, numOtherPhase)
 
 	// ToDo: Complete this function. See the description in the assignment.
+	taskCompleteChan := make(chan bool) // Channel that receives signal that file is done
+	tasksDoneChan := make(chan bool)    // Channel signaling that all work is done
 
-	debug("Schedule: %v phase finished\n", phase)
+	// Thread that monitors task completions
+	go monitorFileCompletion(ntasks, taskCompleteChan, tasksDoneChan)
+
+	// Fire up worker threads
+	for i := range ntasks {
+		workerArgs := RunTaskArgs{
+			JobName:       mr.jobName,
+			File:          mr.files[i],
+			Phase:         phase,
+			TaskNumber:    i,
+			NumOtherPhase: numOtherPhase,
+		}
+
+		go runTask(workerArgs, mr.registerChannel, taskCompleteChan)
+	}
+
+	<-tasksDoneChan
 }
